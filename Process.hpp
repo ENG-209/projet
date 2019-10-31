b811b2a982098660e243db7a8e6e035824b79fa5
Tristan Torchet <tristan.torchet@gmail.com>
2019-10-31 14:45:36 +0100

    Pb 1

--- Process.hpp
+++ Process.hpp
@@ -1,12 +1,12 @@
 /*
- * processus.h
+ * Process.h
  *
  *  Created on: Oct 21, 2019
  *      Author: pcoo59
  */
 
-#ifndef SOURCES_PROCESSUS_H_
-#define SOURCES_PROCESSUS_H_
+#ifndef SOURCES_PROCESS_H_
+#define SOURCES_PROCESS_H_
 
 class Process{
    
@@ -14,18 +14,19 @@ public:
    
    
 protected: 
+   
+
+
+
+
+
+
+
 
 };
 
 
 
 
-
-
-
-};
-
-
-
-
-#endif /* SOURCES_PROCESSUS_H_ */
+#endif /* SOURCES_PROCESS
+_H_ */
